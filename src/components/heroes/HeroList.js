ffb8800cf8bccf2f78f52b9dac2c4299efc7c62a
RafaelCV <cavr134@gmail.com>
2021-06-09 13:28:38 -0500

    first commit

---
+++ src/components/heroes/HeroList.js
@@ -0,0 +1,19 @@
+import React, { useMemo } from 'react'
+import { getHeroesByPublisher } from '../../selectors/getHeoresByPublisher'
+import { HeroCard } from './HeroCard';
+
+export const HeroList = ( { publisher} ) => {
+    const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+    //const heroes = getHeroesByPublisher(publisher);
+    return (
+        <div className="card-columns animate__animated animate__bounce">
+            {
+                heroes.map( hero => (
+                    <HeroCard key={ hero.id }
+                       {...hero} 
+                    />
+                ))
+            }
+        </div>
+    )
+}
---
+++ src/components/marvel/MarvelScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HeroList } from '../heroes/HeroList'
+
+export const MarvelScreen = () => {
+    return (
+        <div className="container mt-5">
+            <h1>MarvelScreen</h1>
+            <hr/>
+            <HeroList publisher="Marvel Comics"/>
+        </div>
+    )
+}
